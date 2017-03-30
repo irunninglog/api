8920100c7ccb2c3f3f61172c7cb30ef53b2ef838
allan-lewis <allan.e.lewis@gmail.com>
2017-03-29 22:32:06 -0400

    #52 - Security re-factor

--- irunninglog-spring/src/test/java/com/irunninglog/spring/workout/RequestResponseTest.java
+++ irunninglog-spring/src/test/java/com/irunninglog/spring/workout/RequestResponseTest.java
@@ -1,7 +1,6 @@
 package com.irunninglog.spring.workout;
 
-import com.irunninglog.api.workout.IGetWorkoutsRequest;
-import com.irunninglog.api.workout.IGetWorkoutsResponse;
+import com.irunninglog.api.workout.*;
 import com.irunninglog.spring.AbstractTest;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
@@ -22,16 +21,26 @@ public class RequestResponseTest extends AbstractTest {
 
     @Test
     public void request() {
-        IGetWorkoutsRequest request = applicationContext.getBean(IGetWorkoutsRequest.class).setDate("date");
+        IGetWorkoutsRequest get = applicationContext.getBean(IGetWorkoutsRequest.class).setDate("date");
+        assertEquals("date", get.getDate());
 
-        assertEquals("date", request.getDate());
+        IPutWorkoutRequest put = applicationContext.getBean(IPutWorkoutRequest.class).setWorkout(null);
+        assertNull(put.getWorkout());
+
+        IDeleteWorkoutRequest delete = applicationContext.getBean(IDeleteWorkoutRequest.class).setWorkoutId(1);
+        assertEquals(1, delete.getWorkoutId());
     }
 
     @Test
     public void response() {
-        IGetWorkoutsResponse response = applicationContext.getBean(IGetWorkoutsResponse.class);
+        IGetWorkoutsResponse get = applicationContext.getBean(IGetWorkoutsResponse.class);
+        assertNull(get.getBody());
 
-        assertNull(response.getBody());
+        IPutWorkoutResponse put = applicationContext.getBean(IPutWorkoutResponse.class);
+        assertNull(put.getBody());
+
+        IDeleteWorkoutResponse delete = applicationContext.getBean(IDeleteWorkoutResponse.class);
+        assertNull(delete.getBody());
     }
 
 }
