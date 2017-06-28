2807a411ba54dd8bb6b7b433efbcdbc4acbc91c4
irunninglog <allan@irunninglog.com>
2017-06-27 22:37:15 -0400

    #76 - API streaks endpoint

--- irunninglog-spring/src/test/java/com/irunninglog/spring/streaks/StreaksServiceTest.java
+++ irunninglog-spring/src/test/java/com/irunninglog/spring/streaks/StreaksServiceTest.java
@@ -1,14 +1,11 @@
 package com.irunninglog.spring.streaks;
 
-import com.irunninglog.api.Progress;
-import com.irunninglog.api.streaks.IStreak;
 import com.irunninglog.api.streaks.IStreaks;
 import com.irunninglog.api.streaks.IStreaksService;
 import com.irunninglog.spring.AbstractTest;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 public class StreaksServiceTest extends AbstractTest {
@@ -26,14 +23,5 @@ public class StreaksServiceTest extends AbstractTest {
     public void getStreaks() {
         IStreaks streaks = streaksService.getStreaks(null);
         assertNotNull(streaks);
-
-        IStreak longest = streaks.getLongest();
-        assertNotNull(longest);
-        assertEquals("2015-01-01", longest.getStartDate());
-        assertEquals("2015-10-01", longest.getEndDate());
-        assertEquals(274, longest.getDays());
-        assertEquals(274, longest.getRuns());
-        assertEquals(100, longest.getPercentage());
-        assertEquals(Progress.GOOD, longest.getProgress());
     }
 }
