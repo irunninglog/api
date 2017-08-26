f17da922aa5e49bcca81b0d488cbbc2004df177c
irunninglog <allan@irunninglog.com>
2017-08-25 20:33:52 -0400

    #97 - Automated deployment

--- app_start.sh
+++ app_start.sh
@@ -1,1 +1,3 @@
+touch /tmp/start
+
 sudo java -Dinstance=IRUNNINGLOGAPI -Dstrava="1|1" -jar irunninglog-main/target/irunninglog.jar > /dev/null 2>&1 &
--- app_stop.sh
+++ app_stop.sh
@@ -1,3 +1,5 @@
+touch /tmp/stop
+
 sudo pkill -9 -f IRUNNINGLOGAPI
 
 exit 0
