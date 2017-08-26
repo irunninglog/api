9d58d3a614c86b2696faab251588d06be6f710cf
irunninglog <allan@irunninglog.com>
2017-08-26 07:37:32 -0400

    #97 - Automated deployment

--- app_start.sh
+++ app_start.sh
@@ -1,4 +1,3 @@
 touch /opt/irunninglog/start
 
-#java -Dinstance=IRUNNINGLOGAPI -Dstrava="1|1" -jar irunninglog-main/target/irunninglog.jar > /dev/null 2>&1 &
 java -Dinstance=IRUNNINGLOGAPI -Dstrava="1|1" -jar /opt/irunninglog/irunninglog.jar > /dev/null 2>&1 &
