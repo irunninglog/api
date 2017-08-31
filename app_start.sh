d47c67173e157bd9314d7528b65770cc2c0ae744
irunninglog <allan@irunninglog.com>
2017-08-31 12:23:19 -0400

    #98 - Logback should be used for logging

--- app_start.sh
+++ app_start.sh
@@ -1,3 +1,3 @@
 touch /opt/irunninglog/api/start
 
-java -Dinstance=IRUNNINGLOGAPI -Dstrava="1|1" -jar /opt/irunninglog/api/irunninglog.jar > /dev/null 2>&1 &
+java -Dlogback.configurationFile=/opt/irunninglog/api/logback.xml -Dinstance=IRUNNINGLOGAPI -Dstrava="1|1" -jar /opt/irunninglog/api/irunninglog.jar > /dev/null 2>&1 &
