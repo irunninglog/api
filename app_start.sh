2081722459c82e01966ea7d1b60bfad63a0fa96e
irunninglog <allan@irunninglog.com>
2017-08-31 16:07:19 -0400

    #97 - Automated deployment

--- app_start.sh
+++ app_start.sh
@@ -1,3 +1,5 @@
 touch /opt/irunninglog/api/start
 
-java -Dlogback.configurationFile=/opt/irunninglog/api/logback.xml -Dinstance=IRUNNINGLOGAPI -Dstrava="1|1" -jar /opt/irunninglog/api/irunninglog.jar > /dev/null 2>&1 &
+export IRL_STRAVA=`cat /home/ec2-user/.strava`
+
+java -Dlogback.configurationFile=/opt/irunninglog/api/logback.xml -Dinstance=IRUNNINGLOGAPI -Dstrava=${IRL_STRAVA} -jar /opt/irunninglog/api/irunninglog.jar > /dev/null 2>&1 &
