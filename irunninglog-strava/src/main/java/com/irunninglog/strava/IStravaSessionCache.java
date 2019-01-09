0fa9100333fa514a112dcdc9258ec8e507ae4348
irunninglog <allan@irunninglog.com>
2019-01-08 21:58:00 -0500

    #130 - Dependency on Strava Java library has been removed

--- irunninglog-strava/src/main/java/com/irunninglog/strava/IStravaSessionCache.java
+++
@@ -1,9 +0,0 @@
-package com.irunninglog.strava;
-
-public interface IStravaSessionCache {
-
-    IStravaSession create(String token);
-
-    IStravaSession get(String token);
-
-}
--- irunninglog-strava/src/main/java/com/irunninglog/strava/IStravaTokenExchange.java
+++
@@ -1,9 +0,0 @@
-package com.irunninglog.strava;
-
-import javastrava.api.v3.auth.model.Token;
-
-public interface IStravaTokenExchange {
-
-    Token token(String code);
-
-}
